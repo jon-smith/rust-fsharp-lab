b0e382c6f5b5de2fed0907e3717855f670d48470
jon-smith <26364802+jon-smith@users.noreply.github.com>
2025-10-19 19:40:57 +0100

    empty webserver

---
+++ rust/webserver/src/main.rs
@@ -0,0 +1,12 @@
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "🦀🐱🤡" }));
+
+    // todo, remove unwraps and handle errors properly
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
