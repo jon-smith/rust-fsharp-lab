6cbe4712242371ab6b8465ed1cdd99535249ed83
jon-smith <26364802+jon-smith@users.noreply.github.com>
2025-10-19 19:22:11 +0100

    remove cpp dependencies

--- rust/sqlx_lib/build.rs
+++
@@ -1,5 +0,0 @@
-#[allow(unused_must_use)]
-fn main() {
-    cxx_build::bridge("src/lib.rs");
-    println!("cargo:rerun-if-changed=src/lib.rs");
-}
--- rust/sqlx_lib/src/lib.rs
+++ rust/sqlx_lib/src/lib.rs
@@ -7,33 +7,6 @@ use sqlx::types::Json;
 use std::env;
 use tokio::runtime::Runtime;
 
-#[cxx::bridge]
-mod ffi {
-
-    pub struct RowData {
-        pub id: i32,
-        pub info: String,
-        pub data: String,
-        pub time: String,
-    }
-
-    extern "Rust" {
-        fn read_all_rows_ffi() -> Result<Vec<RowData>>;
-    }
-}
-
-fn to_ffi_struct(row_data: RowData) -> ffi::RowData {
-    ffi::RowData {
-        id: row_data.id,
-        info: row_data.info,
-        data: row_data.data.to_string(),
-        time: row_data
-            .time
-            .map(|t| t.to_string())
-            .unwrap_or(String::from("")),
-    }
-}
-
 pub struct RowData {
     pub id: i32,
     pub info: String,
@@ -98,8 +71,4 @@ pub async fn clear_table_and_add_single_row_async() -> Result<i32, sqlx::Error> 
 
 pub fn read_all_rows() -> Result<Vec<RowData>, sqlx::Error> {
     Runtime::new().unwrap().block_on(read_all_rows_async())
-}
-
-fn read_all_rows_ffi() -> Result<Vec<ffi::RowData>, sqlx::Error> {
-    read_all_rows().map(|rows| rows.into_iter().map(to_ffi_struct).collect())
 }
