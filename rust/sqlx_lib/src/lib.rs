7f52f421e61caae946fdd267b71620e485614be5
jon-smith <26364802+jon-smith@users.noreply.github.com>
2025-10-22 15:15:03 +0100

    OpenAPI/scalar on one route using utopia

--- rust/sqlx_lib/src/lib.rs
+++ rust/sqlx_lib/src/lib.rs
@@ -7,11 +7,13 @@ use sqlx::types::Json;
 use sqlx::types::chrono;
 use std::env;
 use tokio::runtime::Runtime;
+use utoipa::{ToSchema, schema};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(ToSchema, Serialize, Deserialize, Debug)]
 pub struct RowData {
     pub id: i32,
     pub info: String,
+    #[schema(value_type = serde_json::Value)]
     pub data: Json<serde_json::Value>,
     pub time: Option<chrono::NaiveDateTime>,
 }
--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -1,5 +1,4 @@
 use axum::{
-    Router,
     extract::{Json, State},
     http::StatusCode,
     routing::get,
@@ -9,11 +8,26 @@ use sqlx::PgPool;
 use sqlx_lib::{RowData, read_all_rows};
 use tokio::net::TcpListener;
 
+use utoipa::OpenApi;
+use utoipa_axum::{router::OpenApiRouter, routes};
+use utoipa_scalar::{Scalar, Servable};
+
 fn internal_error<E>(err: E) -> (StatusCode, String)
 where
     E: std::fmt::Display,
 {
     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+#[utoipa::path(
+        get,
+        path = "/",
+        responses(
+            (status = 200, description = "index", body = String),
+        ),
+    )]
+async fn get_default() -> String {
+    "🦀🐱🤡".to_string()
 }
 
 async fn health_check(State(pool): State<PgPool>) -> Result<String, (StatusCode, String)> {
@@ -31,8 +45,18 @@ async fn get_all_data(
     Ok(Json(rows))
 }
 
+const TAG: &str = "rust_webserver";
+
 #[tokio::main]
 async fn main() {
+    #[derive(OpenApi)]
+    #[openapi(
+        tags(
+            (name = TAG, description = "Some API")
+        )
+    )]
+    struct ApiDoc;
+
     // Load environment variables from .env file if we have it
     dotenv().ok();
 
@@ -42,14 +66,22 @@ async fn main() {
         .await
         .expect("Failed to connect to database");
 
-    let app = Router::new()
-        .route("/", get(|| async { "🦀🐱🤡" }))
+    let app_router = OpenApiRouter::new()
+        .routes(routes!(get_default))
         .route("/health", get(health_check))
         .route("/all", get(get_all_data))
         .with_state(pool);
 
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+        .merge(app_router)
+        .split_for_parts();
+
+    let router = router.merge(Scalar::with_url("/scalar", api));
+
     // todo, remove unwraps and handle errors properly
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, router.into_make_service())
+        .await
+        .unwrap();
 }
